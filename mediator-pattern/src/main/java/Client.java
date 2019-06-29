8a593c1ea33482b47f7bce2650e42495ef79b467
zhaozhenghao1993 <736720794@qq.com>
2019-06-29 21:02:16 +0800

    mediator Pattern

---
+++ mediator-pattern/src/main/java/Client.java
@@ -0,0 +1,133 @@
+/**
+ * 🙃
+ * 🙃
+ * 🙃
+ *
+ * @author:zhaozhenghao
+ * @Email :736720794@qq.com
+ * @date :2019/06/29 20:58
+ * Client.java
+ */
+
+//抽象中介者
+abstract class Mediator {
+    public abstract void componentChanged(Component c);
+}
+
+//具体中介者
+class ConcreteMediator extends Mediator {
+    //维持对各个同事对象的引用
+    public Button addButton;
+    public List list;
+    public TextBox userNameTextBox;
+    public ComboBox cb;
+
+    //封装同事对象之间的交互
+    public void componentChanged(Component c) {
+        //单击按钮
+        if(c == addButton) {
+            System.out.println("--单击增加按钮--");
+            list.update();
+            cb.update();
+            userNameTextBox.update();
+        }
+        //从列表框选择客户
+        else if(c == list) {
+            System.out.println("--从列表框选择客户--");
+            cb.select();
+            userNameTextBox.setText();
+        }
+        //从组合框选择客户
+        else if(c == cb) {
+            System.out.println("--从组合框选择客户--");
+            cb.select();
+            userNameTextBox.setText();
+        }
+    }
+}
+
+
+//抽象组件类：抽象同事类
+abstract class Component {
+    protected Mediator mediator;
+
+    public void setMediator(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    //转发调用
+    public void changed() {
+        mediator.componentChanged(this);
+    }
+
+    public abstract void update();
+}
+
+
+//按钮类：具体同事类
+class Button extends Component {
+    public void update() {
+        //按钮不产生交互
+    }
+}
+
+//列表框类：具体同事类
+class List extends Component {
+    public void update() {
+        System.out.println("列表框增加一项：张无忌。");
+    }
+
+    public void select() {
+        System.out.println("列表框选中项：小龙女。");
+    }
+}
+
+//组合框类：具体同事类
+class ComboBox extends Component {
+    public void update() {
+        System.out.println("组合框增加一项：张无忌。");
+    }
+
+    public void select() {
+        System.out.println("组合框选中项：小龙女。");
+    }
+}
+
+//文本框类：具体同事类
+class TextBox extends Component {
+    public void update() {
+        System.out.println("客户信息增加成功后文本框清空。");
+    }
+
+    public void setText() {
+        System.out.println("文本框显示：小龙女。");
+    }
+}
+
+public class Client {
+    public static void main(String[] args) {
+        //定义中介者对象
+        ConcreteMediator mediator;
+        mediator = new ConcreteMediator();
+
+        //定义同事对象
+        Button addBT = new Button();
+        List list = new List();
+        ComboBox cb = new ComboBox();
+        TextBox userNameTB = new TextBox();
+
+        addBT.setMediator(mediator);
+        list.setMediator(mediator);
+        cb.setMediator(mediator);
+        userNameTB.setMediator(mediator);
+
+        mediator.addButton = addBT;
+        mediator.list = list;
+        mediator.cb = cb;
+        mediator.userNameTextBox = userNameTB;
+
+        addBT.changed();
+        System.out.println("-----------------------------");
+        list.changed();
+    }
+}
